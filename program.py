d421dc3cf89bf92dd2ebb1f4d288d14db56c3c5d
Rohitchavan-09 <chavanrohit7498@gmail.com>
2024-01-27 04:05:04 +0000

    this is my modified file

--- program.py
+++ program.py
@@ -1,5 +1,20 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+# Python program to find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
 else:
-   print("{0} is Odd".format(num)i
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
